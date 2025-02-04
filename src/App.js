df98da0e9ec92a6143199e512038e38b57b03195
VidhiPatel-2003 <vidhip@trentium.net>
2025-02-04 12:56:05 +0530

    add initial file

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import './App.css';
+
+function App() {
+  return (
+   <>
+    <h1>Explore India's Heritage</h1>
+    <div className="container">
+        <div className="row">
+            <div className="card">
+                <img src="https://www.holidify.com/images/compressed/attractions/attr_1590.jpg" alt="rani ki vav" width="250px" height="300px"/>
+                <p><b>Title:-</b><strong>Rani ki vav</strong></p>
+                <p><b>Adress:-</b>Mohan Nagar Socity, Patan, Samalpati, Gujarat 384265</p>
+                <p><b>Information:-</b> Rani Ki Vav is a stepwell situated in the town of Patan in Gujarat, India. It is located on the banks of the Saraswati River. </p>
+            </div>
+            <div className="card">
+                <img src="https://www.gujarattourism.com/content/dam/gujrattourism/images/heritage-sites/hutheesing-jain-temple/gallery/Hutheesing%20Jain%20Temple%20(2).jpg" alt="hutheesing jain temple" width="250px"  height="300px"/>
+                <p><b>Title:-</b><strong>Hutheesing Jain Temple</strong></p>
+            <p><b>Adress:-</b> Hutheesing Jain Temple, Shahibaug Rd., Bardolpura, Madhupura, Ahmedabad, Gujarat 380004</p>
+            <p><b>Information:-</b> Hutheesing Temple is a Jain temple in Ahmedabad in Gujarat, India. It was constructed in 1848 by the Hutheesing family. The temple blends the old Maru-Gurjara temple architecture style with new architectural elements of haveli in its design. </p>
+            </div>
+            <div className="card">
+                <img src="https://image-tc.galaxy.tf/wijpeg-4ryg3h3na04hc4kwiqrta4qo5/laxmi-vilas-palace_standard.jpg?crop=134%2C0%2C1333%2C1000" alt="laxmi-vilas-palace" width="250px"  height="300px"/>
+                <p><b>Title:-</b><strong>Laxmi vilas palace</strong></p>
+                <p><b>Adress:-</b>J N Marg, Moti Baug, Vadodara, Gujarat 390001</p>
+                <p><b>Information:-</b> The Lakshmi Vilas Palace in Vadodara, Gujarat, India, was constructed in 1890 by the Gaekwad family, a prominent Maratha family, who ruled the Baroda State. Major Charles Mant was credited to be the main architect of the palace. </p>
+            </div>
+            <p>add a new para</p>
+        </div>
+    </div>
+   </>
+  );
+}
+
+export default App;
